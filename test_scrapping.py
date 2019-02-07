80599e58628944d7c89e0dbd0d781148b6f908b6
Miguel Ávila Monroy <soyer893@gmail.com>
2019-02-07 13:32:38 +0100

    Add new tests

--- test_scrapping.py
+++ test_scrapping.py
@@ -3,33 +3,41 @@
 import unittest
 from bs4 import BeautifulSoup
 import requests
+import json
 
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:61.0) Gecko/20100101 Firefox/'}
+
+books_urls = {}
+with open('./static/book_sources.json') as json_data:
+    books_urls = json.load(json_data)
 
 class Scrapping(unittest.TestCase):
-    url_lcdl = "https://www.casadellibro.com/libros-mas-vendidos/20?gclid=Cj0KCQjwz_TMBRD0ARIsADfk7hSKfsQ-Ec1IyAu1AdZBNz994EIF6EAyB6FVaTRgWzdsoNMT9rDw0nMaAvQdEALw_wcB"
-    url_fiction = "https://www.fnac.es/n2433/Novela-de-Ciencia-Ficcion-y-Fantasy/Libros-mas-vendidos-Ciencia-Ficcion-y-Fantasy"
-    url_nonfiction = "https://www.fnac.es/c19449/No-ficcion"
-    url_laCentral = "https://www.lacentral.com/web/masvendidos/"
 
     def test_urls_are_up(self):
-        self.assertEqual(self.request_url(self.url_lcdl), 200)
-        self.assertEqual(self.request_url(self.url_fiction), 200)
-        self.assertEqual(self.request_url(self.url_nonfiction), 200)
-        self.assertEqual(self.request_url(self.url_laCentral), 200)
+        print(books_urls)
+        for key, value in books_urls.items():
+            self.assertEqual(self.request_url(value), 200)
 
     def test_laCasaDelLibro_same_structure(self):
-        entries = self.get_entries(self.url_lcdl, 'div', 'class', 'carousel-inner')
+        entries = self.get_entries(books_urls['Casa del libro|Más vendidos'], 'div', 'class', 'carousel-inner')
         entry = entries[0]
         titles = entry.find_all('a', {'class': 'title-link'}, limit=5)
         self.assertTrue(len(titles) > 0)
 
-        entry = entries[1]
-        titles = entry.find_all('a', {'class': 'title-link'}, limit=5)
+    def test_Fnac_same_structure(self):
+        entries = self.get_entries(books_urls['Fnac|Más vendidos'], 'li', 'class', 'clearfix Article-item js-ProductList')
+        entry = entries[0]
+        titles = entry.find_all('a', {'class': 'js-minifa-title'}, limit=5)
+        self.assertTrue(len(titles) > 0)
+
+    def test_LaCentral_same_structure(self):
+        entries = self.get_entries(books_urls['La Central|Más vendidos'], 'ul', 'class', 'resultList')
+        entry = entries[0]
+        titles = entry.find_all('div', {'class': 'content'}, limit=5)
         self.assertTrue(len(titles) > 0)
 
     @staticmethod
     def request_url(url):
-        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:61.0) Gecko/20100101 Firefox/'}
         req = requests.get(url, headers=headers)
 
         status_code = req.status_code
@@ -37,7 +45,6 @@ class Scrapping(unittest.TestCase):
 
     @staticmethod
     def get_entries(url, father_struct, struct, class_name):
-        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:61.0) Gecko/20100101 Firefox/'}
         req = requests.get(url, headers=headers)
 
         status_code = req.status_code
