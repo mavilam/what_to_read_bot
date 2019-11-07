ecf7be4375dd33a9d86962fd928629517b027c8e
Miguel Ávila Monroy <soyer893@gmail.com>
2019-11-07 13:17:52 +0100

    Add amazon test

--- tests/test_scrapping.py
+++ tests/test_scrapping.py
@@ -4,12 +4,17 @@ import unittest
 from bs4 import BeautifulSoup
 import requests
 import json
+import os
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:61.0) Gecko/20100101 Firefox/'}
 
 books_urls = {}
-with open('./static/book_sources.json') as json_data:
-    books_urls = json.load(json_data)
+if os.path.exists('/app/static/book_sources.json'):
+    with open('/app/static/book_sources.json') as json_data:
+        books_urls = json.load(json_data)
+else:
+    with open('../static/book_sources.json') as json_data:
+        books_urls = json.load(json_data)
 
 
 class Scrapping(unittest.TestCase):
@@ -22,22 +27,28 @@ class Scrapping(unittest.TestCase):
             self.assertEqual(http_code, 200)
 
     def test_laCasaDelLibro_same_structure(self):
-        entries = self.get_entries(books_urls['Casa del libro|Juvenil'], 'div', 'class', 'product__info')
+        entries = self.get_entries(books_urls['Casa del libro|Juvenil'], 'html.parser', 'div', 'class', 'product__info')
         self.assertTrue(len(entries) > 0)
         entry = entries[0]
         title = entry.a
         self.assertTrue(title is not None)
 
     def test_Fnac_same_structure(self):
-        entries = self.get_entries(books_urls['Fnac|Más vendidos'], 'div', 'class', 'Article-infoContent')
+        entries = self.get_entries(books_urls['Fnac|Más vendidos'], 'html.parser', 'div', 'class', 'Article-infoContent')
         entry = entries[0]
         titles = entry.find('p', {'class': 'Article-descSub'}).findAll('a', limit=5)
         self.assertTrue(len(titles) > 0)
 
     def test_LaCentral_same_structure(self):
-        entries = self.get_entries(books_urls['La Central|Más vendidos'], 'ul', 'class', 'resultList')
+        entries = self.get_entries(books_urls['La Central|Más vendidos'], 'html.parser', 'ul', 'class', 'resultList')
         entry = entries[0]
         titles = entry.find_all('div', {'class': 'content'}, limit=5)
+        self.assertTrue(len(titles) > 0)
+
+    def test_Amazon_same_structure(self):
+        entries = self.get_entries(books_urls['Amazon|Más vendidos'], 'html5lib', 'span', 'class', 'aok-inline-block zg-item')
+        entry = entries[0]
+        titles = entry.find_all('div', {'class': 'p13n-sc-truncate p13n-sc-line-clamp-1'}, limit=5)
         self.assertTrue(len(titles) > 0)
 
     @staticmethod
@@ -48,12 +59,12 @@ class Scrapping(unittest.TestCase):
         return status_code
 
     @staticmethod
-    def get_entries(url, father_struct, struct, class_name):
+    def get_entries(url, parser, father_struct, struct, class_name):
         req = requests.get(url, headers=headers)
 
         status_code = req.status_code
         if status_code == 200:
-            html = BeautifulSoup(req.text, "html.parser")
+            html = BeautifulSoup(req.text, parser)
             entries = html.find_all(father_struct, {struct: class_name})
             return entries
         else:
